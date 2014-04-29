5d332aafe0d27756f7ff8c0454cfc9f0ae190cbd
Jianxiong Hu <silvesthu@gmail.com>
2014-04-30 06:10:08 +0900

    exercise 1.1 ~ 1.3

---
+++ 1.3.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (sqrsum2of3 x y z) 
+		(cond	( (< x y)
+					(cond (	(< x z) (+ (* z z) (* y y)))
+						  ( else (+ (* x x) (* y y)))
+					)
+				)
+				( else 
+					(cond (	(< y z) (+ (* z z) (* x x)))
+						  ( else (+ (* y y) (* x x)))
+					)
+				)
+		)
+)
+
+(sqrsum2of3 5 6 3)
