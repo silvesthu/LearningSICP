de448a46176c44e58c34e19bd8712ebc249cddde
kenhu <silvesthu@gmail.com>
2014-05-15 19:40:42 +0900

    1.10

--- 1.7.scm
+++ 1.7.scm
@@ -6,8 +6,8 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (good-enough? guess last-guess)
-  (< (abs (- guess last-guess)) 0.0001))
+(define (good-enough? guess last-guess) ; compare to last guess
+  (< (abs (- guess last-guess)) 0.0001)) ; epsilon
 
 (define (square x) (* x x))
 
@@ -17,10 +17,6 @@
           (sqrt-iter (improve guess x) guess x)))
 
 (define (sqrt x) (sqrt-iter 1.0 0 x))
-
-(newline)
-; (sqrt-iter 0.05 0.0001) ; bad epsilon
-; (square 0.01)
 
 (sqrt 2)
 (sqrt 0.0001)
