a9d0b30e06380f35f49a2958c5d8c28657e28772
kenhu <silvesthu@gmail.com>
2014-07-06 19:50:07 +0900

    1.32

---
+++ 1.32.scm
@@ -0,0 +1,33 @@
+#lang scheme
+
+(define (accumulate combiner null-value term a next b)
+		(if (> a b)
+			null-value
+			(combiner	(term a)
+				 	(accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate-iter combiner null-value term a next b)
+	(define (iter a result)
+		(if (> a b)
+				result
+				(iter (next a) (combiner result (term a)))
+		)
+	)
+	(iter a null-value)
+)
+
+(define (sum term a next b)
+	(accumulate + 0 term a next b))
+(define (product term a next b)
+	(accumulate * 1 term a next b))
+
+(sum (lambda (x) x) 1 (lambda (x) (+ x 1)) 100)
+(product (lambda (x) x) 1 (lambda (x) (+ x 1)) 10)
+
+(define (sum-iter term a next b)
+	(accumulate-iter + 0 term a next b))
+(define (product-iter term a next b)
+	(accumulate-iter * 1 term a next b))
+
+(sum-iter (lambda (x) x) 1 (lambda (x) (+ x 1)) 100)
+(product-iter (lambda (x) x) 1 (lambda (x) (+ x 1)) 10)
