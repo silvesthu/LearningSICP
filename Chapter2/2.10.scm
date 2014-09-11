0f971a5850a9a545e5f7a8633aa63f0b37428c95
Jianxiong Hu <silvesthu@gmail.com>
2014-09-12 03:31:30 +0900

    2.9 2.10

---
+++ Chapter2/2.10.scm
@@ -0,0 +1,5 @@
+#lang scheme
+
+ (include "interval.scm")
+
+ (div-interval (make-interval 1 1) (make-interval 0 1))
---
+++ Chapter2/2.9.scm
@@ -0,0 +1,31 @@
+#lang scheme
+
+(include "interval.scm")
+
+(define (width n)
+	(/ (- (upper-bound n) (lower-bound n)) 2))
+
+; (width (add-interval x y))
+
+;(width ((make-interval 	
+;			(+ (lower-bound x) (lower-bound y))
+;			(+ (upper-bound x) (upper-bound y))
+;	) x y))
+
+; (/ (- (+ (upper-bound x) (upper-bound y)) (+ (lower-bound x) (lower-bound y))) 2)
+
+; (/ (+ (- (upper-bound x) (lower-bound x)) (- (upper-bound y) (lower-bound y)))) 2)
+
+; (+ (width x) (width y))
+
+; similiar on substract
+
+; But how to prove some function is not exist ...
+
+(width (make-interval 2 2))
+(width (make-interval 0 2))
+(width (mul-interval (make-interval 2 2) (make-interval 0 2)))
+
+; mul
+
+; div
