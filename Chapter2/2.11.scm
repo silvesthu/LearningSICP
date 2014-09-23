0ff778a7873e1b9723ef31cec62932c698a96c0b
Jianxiong Hu <silvesthu@gmail.com>
2014-09-24 01:12:07 +0900

    2.11

--- Chapter2/2.11.scm
+++ Chapter2/2.11.scm
@@ -7,10 +7,59 @@
 			 (ux (upper-bound x))
 			 (ly (lower-bound y))
 			 (uy (upper-bound y)))
-		(display "")
 		(cond 
-			((and (> lx 0) (> ly 0)) (make-interval (* lx ly) (* ux uy)))
-			((and (> lx 0) (> ly 0)) (make-interval (* lx ly) (* ux uy)))
+			((and (> lx 0) (> ux 0) (> ly 0) (> uy 0)) (make-interval (* lx ly) (* ux uy))) ; ++
+			((and (> lx 0) (> ux 0) (< ly 0) (< uy 0)) (make-interval (* ux uy) (* lx ly))) ; +-
+			((and (< lx 0) (< ux 0) (> ly 0) (> uy 0)) (make-interval (* ux uy) (* lx ly))) ; -+
+			((and (< lx 0) (< ux 0) (< ly 0) (< uy 0)) (make-interval (* lx ly) (* ux uy))) ; --
+
+			((and (< lx 0) (> ux 0) (> ly 0) (> uy 0)) (make-interval (* lx uy) (* ux uy))) ; ?+
+			((and (> lx 0) (> ux 0) (< ly 0) (> uy 0)) (make-interval (* ly ux) (* ux uy))) ; +?
+
+			((and (< lx 0) (> ux 0) (< ly 0) (< uy 0)) (make-interval (* ux uy) (* lx uy))) ; ?-
+			((and (< lx 0) (< ux 0) (< ly 0) (> uy 0)) (make-interval (* ux uy) (* ux ly))) ; -?
+
+			((and (< lx 0) (> ux 0) (< ly 0) (> uy 0)) 
+				(make-interval (min (* lx uy) (* ly ux))
+							   (max (* lx ly) (* ux uy)))
+			) ; ??
 		)
 	)
 )
+
+(display "ben")
+(newline)
+
+(mul-interval-ben (make-interval 1 2) (make-interval 3 4)) ; ++
+(mul-interval-ben (make-interval 1 2) (make-interval -3 -4)) ; +-
+(mul-interval-ben (make-interval -1 -2) (make-interval 3 4)) ; -+
+(mul-interval-ben (make-interval -1 -2) (make-interval -3 -4)) ; -- 
+
+(mul-interval-ben (make-interval -1 2) (make-interval 3 4)) ; ?+
+(mul-interval-ben (make-interval 1 2) (make-interval -3 4)) ; +?
+
+(mul-interval-ben (make-interval -1 2) (make-interval -3 -4)) ; ?-
+(mul-interval-ben (make-interval -1 -2) (make-interval -3 4)) ; -?
+
+(mul-interval-ben (make-interval -1 2) (make-interval -3 4)) ; ??
+(mul-interval-ben (make-interval -5 5) (make-interval 3 4)) ; ??
+
+(display "check")
+(newline)
+
+(mul-interval (make-interval 1 2) (make-interval 3 4)) ; ++
+(mul-interval (make-interval 1 2) (make-interval -3 -4)) ; +-
+(mul-interval (make-interval -1 -2) (make-interval 3 4)) ; -+
+(mul-interval (make-interval -1 -2) (make-interval -3 -4)) ; -- 
+
+(mul-interval (make-interval -1 2) (make-interval 3 4)) ; ?+
+(mul-interval (make-interval 1 2) (make-interval -3 4)) ; +?
+
+(mul-interval (make-interval -1 2) (make-interval -3 -4)) ; ?-
+(mul-interval (make-interval -1 -2) (make-interval -3 4)) ; -?
+
+(mul-interval (make-interval -1 2) (make-interval -3 4)) ; ??
+(mul-interval (make-interval -5 5) (make-interval 3 4)) ; ??
+
+(display "end")
+(newline)
