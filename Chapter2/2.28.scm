5a07ebb3686d5a88be084aac9c870584a07a024b
kenhu <silvesthu@gmail.com>
2014-11-17 13:00:14 +0900

    2.28

--- Chapter2/2.28.scm
+++ Chapter2/2.28.scm
@@ -1,11 +1,25 @@
 #lang scheme
 
+(define (concat a b)
+	(if (pair? a)
+		(cons (car a) (concat (cdr a) b))
+		(if (null? a)
+			b
+			(concat (list a) b)
+		)		
+	)
+)
+
 (define (fringe root)
-	(newline)
+	(if (pair? root)
+		(concat (fringe (car root)) (fringe (cdr root)))
+		root
+	)
 )
 
 (define x (list (list 1 2) (list 3 4)))
 
+(concat (list 1 2) (list 3 4))
 (fringe x)
+(fringe (list (list 1 (list 3 4 (list 5 6) 7) (list 8 9))))
 
-(fringe (list x x))
